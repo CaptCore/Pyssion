49ba68a5a59a839a5c562f14b26e9a2bce643006
jomin-nxt <jmkim@neuroxt.com>
2025-05-21 17:27:42 +0900

    pyssion reactor more improve

--- pyssion_lib/pyssion/reactor_extension/service.py
+++ pyssion_lib/pyssion/reactor_extension/service.py
@@ -1,4 +1,17 @@
-from kubernetes.client import V1Service, V1ServiceSpec, V1ServicePort, V1ObjectMeta
+from kubernetes.client import (
+    V1Service,
+    V1Deployment,
+    V1EnvVar,
+    V1LabelSelector,
+    V1ServicePort,
+    V1ContainerPort,
+    V1ObjectMeta,
+    V1PodSpec,
+    V1ServiceSpec,
+    V1PodTemplateSpec,
+    V1DeploymentSpec,
+    V1Container,
+)
 
 
 def make_service(name: str, selector: dict, port: int):
@@ -20,3 +33,44 @@ def make_service(name: str, selector: dict, port: int):
             selector=selector, ports=[V1ServicePort(port=port, target_port=port)]
         ),
     )
+
+
+def make_deployment(name: str, labels: dict, replicas: int, container: V1Container):
+    template = V1PodTemplateSpec(
+        metadata=V1ObjectMeta(labels=labels), spec=V1PodSpec(containers=[container])
+    )
+    spec = V1DeploymentSpec(
+        replicas=replicas,
+        selector=V1LabelSelector(match_labels=labels),
+        template=template,
+    )
+    return V1Deployment(
+        api_version="apps/v1",
+        kind="Deployment",
+        metadata=V1ObjectMeta(name=name),
+        spec=spec,
+    )
+
+
+def deploy_parameter_server(
+    core_api, apps_api, namespace, port, replica, container_image
+):
+    # Can Exchange this to builder.py -> init_container function?
+    # 1) Service
+    svc = make_service("ps-svc", {"app": "ps"}, port)
+    core_api.create_namespaced_service(namespace, svc)
+    print("✅ Service 'ps-svc' created")
+
+    # 2) Container build
+    # this part will create Parameter Server for Pyssion
+    ps_cmd = "pip install --no-cache-dir Flask numpy && " "python /app/ps_server.py"
+    ps_container = V1Container(
+        name="ps",
+        image=container_image,
+        command=["/bin/sh", "-c", ps_cmd],
+        ports=[V1ContainerPort(container_port=port)],
+        env=[V1EnvVar(name="WORKER_REPLICAS", value=str(replica))],
+    )
+    ps_dep = make_deployment("ps-deploy", {"app": "ps"}, replica, ps_container)
+    apps_api.create_namespaced_deployment(namespace, ps_dep)
+    print(f"✅ Deployment 'ps-deploy' ({replica} replicas) created")
